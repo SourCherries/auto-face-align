a4669cb9ee967bae53fed3ee45a5807623ef00f8
SourCherries <carl.michael.gaspar@icloud.com>
2024-05-24 16:55:12 +0400

    update functions in make_files to rely on pathlib. install and demo works.

--- alignfaces/src/alignfaces/make_files.py
+++ alignfaces/src/alignfaces/make_files.py
@@ -1,80 +1,36 @@
-import glob
+from pathlib import Path
 import os
-
-# Deprecated function (does too much!)
-def make_files(MotherDir, FilePrefix="", FilePostfix="jpg", new_dir="aligned"):
-    # Copy directory structure for output
-    MotherBits = MotherDir.split(os.path.sep)
-    go_back = -len(MotherBits[-2]) - 1
-    GrannyDir = MotherDir[0:go_back]
-    AuntieDir = GrannyDir + MotherBits[-2] + "-" + new_dir + os.path.sep
-
-    if not os.path.isdir(AuntieDir):
-        os.mkdir(AuntieDir)
-
-    input_files = []
-    output_files = []
-    for infile in glob.iglob(MotherDir + "**" + os.path.sep +
-                             FilePrefix + "*." + FilePostfix, recursive=True):
-        input_files.append(infile)
-        GiveToAuntie = infile[len(MotherDir):]
-        output_files.append(AuntieDir + GiveToAuntie)
-        AdoptedBits = GiveToAuntie.split(os.path.sep)
-        AdoptedBits = AdoptedBits[:-1]
-        CheckForThis = AuntieDir
-        for next_dir in AdoptedBits:
-            CheckForThis = CheckForThis + next_dir + os.path.sep
-            if not os.path.isdir(CheckForThis):
-                os.mkdir(CheckForThis)
-
-    assert len(input_files) == len(output_files)
-    # # Write lists to files.
-    # with open(GrannyDir + 'input_files.data', 'wb') as filehandle:
-    #     pickle.dump(input_files, filehandle)
-    # with open(GrannyDir + 'output_files.data', 'wb') as filehandle:
-    #     pickle.dump(output_files, filehandle)
-    files = []
-    files.append(input_files)
-    files.append(output_files)
-    output_dir = AuntieDir
-    return files, output_dir
 
 
 # Run at outset of get_landmarks()
 def get_source_files(MotherDir, FilePrefix="", FilePostfix="jpg"):
-    MotherBits = MotherDir.split(os.path.sep)
+    if type(MotherDir) == str:
+        MotherDir = Path(MotherDir)
     input_files = []
-    for infile in glob.iglob(MotherDir + "**" + os.path.sep +
-                             FilePrefix + "*." + FilePostfix, recursive=True):
-        input_files.append(infile)
+    for p in MotherDir.rglob(FilePrefix + "*" + FilePostfix):
+        input_files.append(str(p))
+        print(str(p))
     return input_files
 
 
 # Run at outset of align_procrustes() and place_aperture()
 def clone_directory_tree(MotherDir, new_dir="aligned", FilePrefix="", FilePostfix="jpg"):
-    MotherBits = MotherDir.split(os.path.sep)
-    go_back = -len(MotherBits[-2]) - 1
-    GrannyDir = MotherDir[0:go_back]
-    AuntieDir = GrannyDir + MotherBits[-2] + "-" + new_dir + os.path.sep
-
-    if not os.path.isdir(AuntieDir):
-        os.mkdir(AuntieDir)
-
-    for infile in glob.iglob(MotherDir + "**" + os.path.sep +
-                             FilePrefix + "*." + FilePostfix, recursive=True):
-
-        GiveToAuntie = infile[len(MotherDir):]
-
-        AdoptedBits = GiveToAuntie.split(os.path.sep)
-        AdoptedBits = AdoptedBits[:-1]
-        CheckForThis = AuntieDir
-        for next_dir in AdoptedBits:
-            CheckForThis = CheckForThis + next_dir + os.path.sep
-            if not os.path.isdir(CheckForThis):
-                os.mkdir(CheckForThis)
-
-    output_dir = AuntieDir
-    return output_dir
+    if type(MotherDir) == str:
+        MotherDir = Path(MotherDir)    
+    input_files = get_source_files(MotherDir, FilePrefix=FilePrefix, FilePostfix=FilePostfix)
+    P = []
+    for f in input_files:
+        P.append(Path(f).parent)
+    P = list(set(P))
+    mothers_name = MotherDir.parts[-1]
+    new_directory = MotherDir.parent / (str(mothers_name) + "-" + new_dir)
+    print(new_directory)
+    for p in P:
+        new_dir = new_directory / p.relative_to(MotherDir)
+        if not new_dir.exists():
+            new_dir.mkdir(parents=False, exist_ok=False) # NEED TEST
+        print(new_dir)
+    return str(new_directory) + os.sep
 
 
 # Within make_aligned_faces (align_procrustes and place_aperture),
